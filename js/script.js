a469989bc2ec83faee8d49314160f49d1dfe8363
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2025-04-07 00:19:32 +0300

    Новый файл 1 февраля 25

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const kmlFiles = [
     { name: "01.11.24", path: "kml/Line_24_11_01.kml" },
     { name: "01.12.24", path: "kml/Line_24_12_01.kml" },
     { name: "01.01.25", path: "kml/Line_25_01_01.kml" }, 
-    { name: "01.02.25", path: "kml/Line_01_02_25.kml" },
+    { name: "01.02.25", path: "kml/Line_25_02_01.kml" },
     { name: "01.03.25", path: "kml/Line_01_03_25.kml" },
     { name: "03.04.25", path: "kml/Line_03_04_25.kml" }
     
