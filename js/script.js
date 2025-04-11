7c05c44dfb0f6494ec1d52a175f0e9129460a8e8
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2025-04-11 12:17:13 +0300

    Update script.js + 1 июля 24

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Основные KML-файлы
 const kmlFiles = [
     { name: "22.02.24", path: "kml/Line_22_02_24.kml" },
+    { name: "01.07.24", path: "kml/Line_24_07_01.kml" },
     { name: "01.08.24", path: "kml/Line_24_08_01.kml" },
     { name: "01.09.24", path: "kml/Line_24_09_01.kml" },
     { name: "01.10.24", path: "kml/Line_24_10_01.kml" },
