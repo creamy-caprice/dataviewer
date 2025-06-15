b4636d7dfeafab1cbc20f420248760d3279c04db
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2025-06-16 01:16:36 +0300

    Возвращены зоны контроля за май

--- js/script.js
+++ js/script.js
@@ -6,15 +6,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Основные KML-файлы
 window.kmlFiles = [
-    { name: "01.10.24", path: "kml/Line_24_10_01.kml" },
-    { name: "01.11.24", path: "kml/Line_24_11_01.kml" },
-    { name: "01.12.24", path: "kml/Line_24_12_01.kml" },
-    { name: "01.01.25", path: "kml/Line_25_01_01.kml" }, 
-    { name: "01.02.25", path: "kml/Line_25_02_01.kml" },
-    { name: "01.03.25", path: "kml/Line_25_03_01.kml" },
-    { name: "03.04.25", path: "kml/Line_25_04_03.kml" }
-    
-];
+	{ name: "01.02.25", path: "kml/Control_25_02_01.kml" },
+    	{ name: "01.03.25", path: "kml/Control_25_03_01.kml" },
+   	{ name: "01.04.25", path: "kml/Control_25_04_01.kml" },
+    	{ name: "01.05.25", path: "kml/Control_25_05_01.kml" },
+	{ name: "02.05.25", path: "kml/Control_25_05_02.kml" },
+	{ name: "06.05.25", path: "kml/Control_25_05_06.kml" },
+	{ name: "10.05.25", path: "kml/Control_25_05_10.kml" },
+	{ name: "14.05.25", path: "kml/Control_25_05_14.kml" },
+	{ name: "18.05.25", path: "kml/Control_25_05_18.kml" },
+	{ name: "22.05.25", path: "kml/Control_25_05_22.kml" },
+	{ name: "26.05.25", path: "kml/Control_25_05_26.kml" }  
+	];
 
 // Постоянный слой
 const permanentLayerData = {
