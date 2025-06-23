bf6b955db5feda2c4f1d925dac6f7a4678137bbe
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2025-06-24 02:04:15 +0300

    Update data.js изменены пути

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 // Основные KML-файлы
 window.kmlFiles = [
-    { name: "01.02.25", path: "kml/Control_25_02_01.kml" },
-    { name: "01.03.25", path: "kml/Control_25_03_01.kml" },
-    { name: "01.04.25", path: "kml/Control_25_04_01.kml" },
+    { name: "01.02.25", path: "kml/ControlZones/Control_25_02_01.kml" },
+    { name: "01.03.25", path: "kml/ControlZones/Control_25_03_01.kml" },
+    { name: "01.04.25", path: "kml/ControlZones/Control_25_04_01.kml" },
     { name: "01.05.25", path: "kml/ControlZones/Control_25_05_01.kml" },
     { name: "02.05.25", path: "kml/ControlZones/Control_25_05_02.kml" },
     { name: "06.05.25", path: "kml/ControlZones/Control_25_05_06.kml" },
