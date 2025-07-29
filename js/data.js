47a6e57dfc3634dc41579ce21df7be1dc7a96e8d
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2025-07-29 12:27:19 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -53,7 +53,7 @@ window.kmlFiles = [
 // Постоянные слои
 window.permanentLayers = [
     // { name: "LDPR", path: "kml/PermanentObjects/LDPR.kml" },
-    { name: "LDPR", path: "kml/PermanentObjects/LDPR_line.kml" },
+    { name: "LDPR", path: "kml/PermanentObjects/LDNR_line.kml" },
     { name: "RuUaBorder", path: "kml/PermanentObjects/RuUaBorder.kml" },
     // { name: "Offensive_23_10_10", path: "kml/PermanentObjects/FrontLine_23_10_06.kml" },
     { name: "Current_frontline", path: "kml/FrontLine/FrontLine_25_07_25.kml" },
