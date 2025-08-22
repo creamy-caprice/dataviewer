d1b53ef2e709c6f6f445c5412a86bf3874bdbf62
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2025-08-22 10:13:39 +0300

    Добавлена дата

--- js/data.js
+++ js/data.js
@@ -52,7 +52,8 @@ window.kmlFiles = [
     { name: "06.08.25", path: "kml/ControlZones/Control_25_08_06.kml" },
     { name: "10.08.25", path: "kml/ControlZones/Control_25_08_10.kml" },
     { name: "14.08.25", path: "kml/ControlZones/Control_25_08_14.kml" },
-    { name: "18.08.25", path: "kml/ControlZones/Control_25_08_18.kml" }
+    { name: "18.08.25", path: "kml/ControlZones/Control_25_08_18.kml" },
+    { name: "22.08.25", path: "kml/ControlZones/Control_25_08_22.kml" }
 ];
 
 // Постоянные слои
@@ -65,7 +66,7 @@ window.permanentLayers = [
     { name: "RuUaBorder",             path: "kml/PermanentObjects/5.kml" },
     // { name: "Offensive_23_10_10", path: "kml/PermanentObjects/FrontLine_23_10_06.kml" },
     // Текущая ЛБС
-    { name: "Current_frontline",      path: "kml/FrontLine/FrontLine_25_08_14.kml" },
+    { name: "Current_frontline",      path: "kml/FrontLine/FrontLine_25_08_18.kml" },
     // Составная линия на 10.10.23
     //{ name: "Offensive_23_10_10",     path: "kml/PermanentObjects/тест2-101023.kml" },
     //{ name: "Offensive_23_10_10_add", path: "kml/PermanentObjects/тест3_101023_add.kml" },
@@ -89,6 +90,7 @@ window.cities = [
     { name: { ru: "Гуляйполе", en: "Gulyaypole" }, lat: 47.66336,  lng:  36.2587   },
     { name: { ru: "Орехов",    en: "Orekhov"    }, lat: 47.5675,   lng:  35.78845  },
 ];
+
 
 
 
