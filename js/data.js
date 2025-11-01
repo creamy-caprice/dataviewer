f6a653b850659878803e5c0489d023d8e001293a
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2025-11-01 23:51:49 +0300

    Добавлена дата

--- js/data.js
+++ js/data.js
@@ -71,12 +71,13 @@ window.kmlFiles = [
     { name: "21.10.25", path: "kml/ControlZones/Control_25_10_21.kml" },
     { name: "25.10.25", path: "kml/ControlZones/Control_25_10_25.kml" },
     { name: "29.10.25", path: "kml/ControlZones/Control_25_10_29.kml" }
+	{ name: "02.11.25", path: "kml/ControlZones/Control_25_11_02.kml" }
 ];
 
 // Постоянные слои
 window.permanentLayers = [
     // Текущая ЛБС
-    { name: "Current_frontline",      path: "kml/FrontLine/FrontLine_25_10_29.kml" },
+    { name: "Current_frontline",      path: "kml/FrontLine/FrontLine_25_11_02.kml" },
     // Изменения по сводке
     // { name: "Progress",      path: "kml/PermanentObjects/Progress_25_10_09.kml" },
     // Граница ЛДНР без Ростовской области
@@ -109,6 +110,7 @@ window.cities = [
     { name: { ru: "Орехов",     en: "Orekhov"    }, lat: 47.5675,   lng:  35.78845  },
     { name: { ru: "Степногорск", en: "Stepnogorsk" }, lat: 47.588488,   lng:  35.362448 },
 ];
+
 
 
 
