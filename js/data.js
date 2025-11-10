b63e65bbec16acbd242f2a367fabf686e42bc5a6
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2025-11-10 09:19:59 +0300

    Добавлена дата

--- js/data.js
+++ js/data.js
@@ -267,11 +267,11 @@ window.kmlFiles = [
                                 "kml/Progress/RuAF/ProgressRuAF_25_11_06.kml",
 								"kml/FrontLine/FrontLine_25_11_06.kml",
                                 "kml/Progress/AFU/ProgressAFU_25_11_06.kml"] },
-	//{name: "10.11.25", paths: [
-    //                          "kml/ControlZones/Control_25_11_10.kml",
-    //                        "kml/Progress/RuAF/ProgressRuAF_25_11_10.kml",
-	//							"kml/FrontLine/FrontLine_25_11_10.kml",
-    //                            "kml/Progress/AFU/ProgressAFU_25_11_10.kml"] }
+	{name: "10.11.25", paths: [
+        	                    "kml/ControlZones/Control_25_11_10.kml",
+    	                        "kml/Progress/RuAF/ProgressRuAF_25_11_10.kml",
+								"kml/FrontLine/FrontLine_25_11_10.kml",
+                                "kml/Progress/AFU/ProgressAFU_25_11_10.kml"] }
    ];
 
 // Постоянные слои
@@ -310,6 +310,7 @@ window.cities = [
     { name: { ru: "Орехов",     en: "Orekhov"    }, lat: 47.5675,   lng:  35.78845  },
     { name: { ru: "Степногорск", en: "Stepnogorsk" }, lat: 47.588488,   lng:  35.362448 },
 ];
+
 
 
 
