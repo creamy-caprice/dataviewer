678027c51eed514e81e70f66514cea93ecb2de4f
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2026-02-03 02:04:32 +0300

    Изменен Прогресс за 24 января

--- js/data.js
+++ js/data.js
@@ -364,9 +364,10 @@ window.kmlFiles = [
                                 "kml/Progress/AFU/ProgressAFU_26_01_21.kml"] },
 	{name: "25.01.26", paths: [
         	                    "kml/ControlZones/Control_26_01_25.kml",
-    	                        "kml/Progress/RuAF/ProgressRuAF_26_01_25.kml",
+    	                        //"kml/Progress/RuAF/ProgressRuAF_26_01_25.kml",
 								"kml/FrontLine/FrontLine_26_01_25.kml",
-                                "kml/Progress/AFU/ProgressAFU_26_01_25.kml"] },
+                                //"kml/Progress/AFU/ProgressAFU_26_01_25.kml"
+								"kml/Progress/Progress_26_01_25.kml"] },
 	{name: "29.01.26", paths: [
         	                    "kml/ControlZones/Control_26_01_29.kml",
     	                        //"kml/Progress/RuAF/ProgressRuAF_26_01_29.kml",
@@ -439,6 +440,7 @@ window.milequipLayers = [];
 window.attacksOnUaKmlPaths = ["kml/Geolocations/osint_alarm_2025.kml",
                               "kml/Geolocations/osint_alarm_2026.kml"];
 window.attacksOnUaLayers = [];
+
 
 
 
