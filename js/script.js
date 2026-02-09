00676c2241bc2e73370044cec9ccf7e99025956e
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2026-02-10 01:02:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1471,6 +1471,7 @@ function getAttacksOnUaIcon(position) {
         'Тяговая подстанция'                               : 'img/attack types/тяговая подстанции.png',
         'Мост'                                             : 'img/attack types/мосты.png',
         'Газовая инфраструктура'                           : 'img/attack types/газ.png',
+        'Объект неустановленного назначения'               : 'img/attack types/вопрос.png',
         'default'                                          : 'img/attack types/Взрывчик.png',
     };
 
@@ -3644,6 +3645,7 @@ document.addEventListener('click', function(e) {
         }
     }
 });
+
 
 
 
