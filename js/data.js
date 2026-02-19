af1d0eaa950c3124df278714dd0ca4aff44e0c38
сливочный каприз <146118406+creamy-caprice@users.noreply.github.com>
2026-02-19 22:54:22 +0300

    Добавлены файлы траншей Лиман и Купянск

--- js/data.js
+++ js/data.js
@@ -133,13 +133,17 @@ window.attacksOnUaKmlPaths = ["kml/Geolocations/osint_alarm_2022.kml",
 window.attacksOnUaLayers = [];
 
 
-window.fortificationKmlPaths = ["kml/Fortifications/Trenches_Pokrovsk.kml",
-								"kml/Fortifications/Trenches_ Mezhevaya.kml",
+window.fortificationKmlPaths = ["kml/Fortifications/Trenches_Kupyansk.kml
+								"kml/Fortifications/Trenches_Lyman.kml",
 								"kml/Fortifications/Trenches_Kramatorsk.kml",
+								"kml/Fortifications/Trenches_Pokrovsk.kml",
+								"kml/Fortifications/Trenches_ Mezhevaya.kml",
+								
 								"kml/Fortifications/Ditches.kml",
 								"kml/Fortifications/Barbed_wire.kml",
                                 "kml/Fortifications/Dragons_teeth.kml"];
 window.fortificationLayers = [];
+
 
 
 
