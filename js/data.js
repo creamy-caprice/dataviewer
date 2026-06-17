5ba8838628e8312760ebe9955f559692fa19c250
сливочный каприз <georges.crecap@gmail.com>
2026-06-17 21:11:23 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -73,7 +73,7 @@ window.kmlFiles = (window.dateList || []).map(dateStr => {
 // Постоянные слои
 window.permanentLayers = [
     // Текущая ЛБС
-    { name: "Current_frontline",      path: "kml/FrontLine/FrontLine_26_06_14.kml" },
+    { name: "Current_frontline",      path: "kml/FrontLine/FrontLine_26_06_18.kml" },
       // Граница ЛДНР без Ростовской области
     { name: "LDPR",                   path: "kml/PermanentObjects/LDPR_Border.kml" },
     //Конституционная граница РФ
